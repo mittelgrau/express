a4e3596c9840c9b84ed1e72df170004ec643eb93
Glitch (canary-sphere) <none>
2019-09-29 21:48:15 +0000

    🍪💐 Checkpoint ./routes/github.js:6368029/634 ./server.js:6368029/228

--- routes/github.js
+++ routes/github.js
@@ -1,60 +1,34 @@
 const express = require('express');
 const got = require('got');
 const router = express.Router();
-const {fileToJson} = require('../helpers.js')
-
-
-
-
+const { fileToJson } = require('../helpers.js');
 
 router.get('/entries', async (req, res) => {
-
   const url =
-        'https://api.github.com/repos/mittelgrau/node_db/contents/_data?ref=master';
-    const raw = await got(url, {
-        headers: {
-            Authorization: `token ${token}`
-        }
-    });
-
-    const download_links = JSON.parse(raw.body).map(item => item.download_url);
-
-    const files = await download_links.reduce(async (acc, url) => {
-        const collection = await acc;
-        let page = await got(url);
-        let body = page.body;
-        let pageObject = fileToJson(body);
-
-        collection.push(pageObject);
-
-        return collection;
-    }, Promise.resolve([]));
-
-  res.send(files)
-  
-});
-
-const postFile = async data => {
-  const url =
-    'https://api.github.com/repos/mittelgrau/node_db/contents/db.json';
-  const payload = {
-    message: 'new shared link',
-    content: Buffer.from(fileContent).toString('base64'),
-    comitter: {
-      name: '',
-      email: ''
-    }
-  };
-
-  const options = {
-    method: 'PUT',
+    'https://api.github.com/repos/mittelgrau/node_db/contents/_data?ref=master';
+  const raw = await got(url, {
     headers: {
       Authorization: `token ${process.env.GITTOKEN}`
-    },
-    body: Json.stringify(payload)
-  };
+    }
+  });
 
-  return await fetch(url, options);
+  const download_links = JSON.parse(raw.body).map(item => item.download_url);
+
+  const files = await download_links.reduce(async (acc, url) => {
+    const collection = await acc;
+    let page = await got(url);
+    let body = page.body;
+    let pageObject = fileToJson(body);
+
+    collection.push(pageObject);
+
+    return collection;
+  }, Promise.resolve([]));
+
+  res.send(files);
+});
+
+;
 };
 
 async function testing() {
--- server.js
+++ server.js
@@ -3,26 +3,26 @@ const app = express();
 const morgan = require('morgan');
 const cors = require('cors');
 const cookie = require('cookie-parser');
-const argon2 = require('argon2');
+const helmet = require('helmet');
 
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(morgan('tiny'));
-app.use(helmet());
-app.use(cors());
-app.use()
 
+app.use(cors());
+app.use();
 
 app.get('/', (req, res) => {
   res.send('test');
 });
 
-app.post('/authenticate', (req,res) => {
+const gitRouter = require('./routes/github');
+app.use('/api', gitRouter);
+
+app.post('/authenticate', (req, res) => {
   res.send(req.body);
 });
-
-
 
 const listener = app.listen(process.env.PORT, function() {
   console.log('Your app is listening on port ' + listener.address().port);
