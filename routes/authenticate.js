b2004b474723aa9a0f3f15b29ccee1a2d9743127
Glitch (canary-sphere) <none>
2019-09-29 22:10:36 +0000

    🌹🏚 Checkpoint ./package.json:6368029/5042 ./routes/authenticate.js:6368029/951

--- routes/authenticate.js
+++ routes/authenticate.js
@@ -1,16 +1,46 @@
 const express = require('express');
 const router = express.router();
 const argon2 = require('argon2');
+const nanoid = require('nanoid');
+const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const cookie = require('cookie-parser');
+const { celebrate, Joi, errors } = require('celebrate');
 
 async function decrypt(password) {
   try {
     return (verify = await argon2.verify(process.env.PASSWORD, password));
   } catch (err) {
-    return error;
+    return false;
   }
-};
+}
+
+router.post(
+  'login',
+  celebrate({
+    body: Joi.object().keys({
+      password: Joi.string().required()
+    })
+  }),
+  (req, res) => {
+    if (!decrypt(req.body.password)) return res.status(403).send('not allowed');
+
+    const payload = {
+      id: nanoid()
+    };
+
+    const token = jwt.sign(payload, process.env.JWT_SECRET);
+    
+    const cookieOptions = {
+        httpOnly: true,
+        expires: 0
+    }
+
+    res.cookie('auth_token', token, cookieOptions);
+    res.
+
+  }
+);
 
 
 
