327c3ec5ce9955a672752d700066818799e1ac89
Glitch (canary-sphere) <none>
2019-09-30 17:13:36 +0000

    🎎🎖 Checkpoint ./routes/authenticate.js:6368029/265

--- routes/authenticate.js
+++ routes/authenticate.js
@@ -8,49 +8,52 @@ const cookie = require('cookie-parser');
 const { celebrate, Joi, errors } = require('celebrate');
 
 async function decrypt(password) {
-  try {
-    return await argon2.verify(process.env.PASSWORD, password);
-  } catch (err) {
-    return false;
-  }
+    try {
+        return await argon2.verify(process.env.PASSWORD, password);
+    } catch (err) {
+        return false;
+    }
 }
 
 router.post(
-  '/login',
-  celebrate({
-    body: Joi.object().keys({
-      password: Joi.string().required()
-    })
-  }),
-  (req, res) => {
-    if (!decrypt(req.body.password)) return res.status(403).send('not allowed');
+    '/login',
+    celebrate({
+        body: Joi.object().keys({
+            password: Joi.string().required()
+        })
+    }),
+    (req, res) => {
+        console.log(decrypt(req.body.password));
+        if (!decrypt(req.body.password)) {
+            return res.status(403).send('not allowed');
+        }
 
-    const payload = {
-      id: nanoid()
-    };
+        const payload = {
+            id: nanoid()
+        };
 
-    const token = jwt.sign(payload, process.env.JWT_SECRET);
+        const token = jwt.sign(payload, process.env.JWT_SECRET);
 
-    const cookieOptions = {
-      httpOnly: true,
-      expires: 0
-    };
+        const cookieOptions = {
+            httpOnly: true,
+            expires: 0
+        };
 
-    res.cookie('auth_token', token, cookieOptions);
-    res.status(401).send('okay');
-  }
+        res.cookie('auth_token', token, cookieOptions);
+        res.status(200).send('okay');
+    }
 );
 
 router.get('/test', (req, res) => {
-  const cooki = req.cookies;
-  // const verified = jwt.verify(req.cookies, process.env.JWT_SECRET, function(err,token){
-  // if(err) {
-  //  res.status(403).send('Your authentication seems to be wrong);
-  //} else{
-  //res.send('hiiiii bro')
-  //}
-  //})
-  res.status(403).send(cooki);
+    const cooki = req.cookies;
+    // const verified = jwt.verify(req.cookies, process.env.JWT_SECRET, function(err,token){
+    // if(err) {
+    //  res.status(403).send('Your authentication seems to be wrong);
+    //} else{
+    //res.send('hiiiii bro')
+    //}
+    //})
+    res.status(403).send(cooki);
 });
 
 module.exports = router;
