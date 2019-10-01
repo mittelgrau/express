9f2956959a4c7f07d4965da5436db3df2de733ec
Glitch (canary-sphere) <none>
2019-10-01 15:15:44 +0000

    😄👓 Checkpoint ./routes/authenticate.js:6368029/293 ./package.json:6368029/3742 ./controller/validate.js:6368029/311

---
+++ controller/validate.js
@@ -0,0 +1,10 @@
+const Joi = require('@hapi/joi');
+
+const loginValidation = data => {
+    const schema = {
+        password: Joi.string().required()
+    };
+    return Joi.validate(data, schema);
+};
+
+module.exports.loginValidation = loginValidation;
--- routes/authenticate.js
+++ routes/authenticate.js
@@ -7,33 +7,26 @@ require('dotenv').config();
 const cookie = require('cookie-parser');
 const { celebrate, Joi, errors } = require('celebrate');
 
-router.post(
-    '/login',
-    celebrate({
-        body: Joi.object().keys({
-            password: Joi.string().required()
-        })
-    }),
-    async (req, res) => {
-        if (req.body.password !== process.env.SAMPLEPASSWORD) {
-            return res.status(403).send('not allowed');
-        }
-
-        const payload = {
-            id: nanoid()
-        };
-
-        const token = jwt.sign(payload, process.env.JWT_SECRET);
-
-        const cookieOptions = {
-            httpOnly: true,
-            expires: 0
-        };
-
-        res.cookie('auth_token', token, cookieOptions);
-        res.status(200).send('okay');
+router.post('/login', async (req, res) => {
+    // {error} = loginValidation(req.body);
+    if (req.body.password !== process.env.SAMPLEPASSWORD) {
+        return res.status(403).send('not allowed');
     }
-);
+
+    const payload = {
+        id: nanoid()
+    };
+
+    const token = jwt.sign(payload, process.env.JWT_SECRET);
+
+    const cookieOptions = {
+        httpOnly: true,
+        expires: 0
+    };
+
+    res.cookie('auth_token', token, cookieOptions);
+    res.status(200).send('okay');
+});
 
 router.get('/test', (req, res) => {
     // const verified = jwt.verify(req.cookies, process.env.JWT_SECRET, function(err,token){
