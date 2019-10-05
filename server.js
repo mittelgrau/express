706d3ca171386cbda9425a0be99f834d775e06ef
Glitch (canary-sphere) <none>
2019-10-05 14:25:57 +0000

    🚙🛍 Checkpoint ./server.js:6368029/79

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ const jwt = require('jsonwebtoken');
 app.use(cors({ credentials: true, origin: 'http://localhost:3000' }));
 
 app.get('/', (req, res) => {
-  res.send('test');
+    res.send('test');
 });
 
 const authRouter = require('./routes/authenticate');
@@ -28,37 +28,38 @@ app.use('/auth', authRouter);
 // req.cookies
 
 app.post('/login', async (req, res) => {
-  // {error} = loginValidation(req.body);
-  if (req.body.password !== process.env.SAMPLEPASSWORD) {
-    return res.status(403).send('not allowed');
-  }
+    // {error} = loginValidation(req.body);
+    if (req.body.password !== process.env.SAMPLEPASSWORD) {
+        return res.status(403).send('not allowed');
+    }
 
-  const payload = {
-    id: nanoid()
-  };
+    const payload = {
+        id: nanoid()
+    };
 
-  const token = jwt.sign(payload, process.env.JWT_SECRET);
+    const token = jwt.sign(payload, process.env.JWT_SECRET);
 
-  res.cookie('auth_token', token, {
-    httpOnly: false,
-    path: '/'
-  });
+    res.cookie('auth_token', token, {
+        httpOnly: false,
+        sameSite: false,
+        path: '/'
+    });
 
-  res.status(200).send(req.cookies);
+    res.status(200).send(req.cookies);
 });
 
 app.get('/test', async (req, res) => {
-  const token = req.cookies;
-  res.send(token);
+    const token = req.cookies;
+    res.send(token);
 });
 
 const gitRouter = require('./routes/github');
 app.use('/api', gitRouter);
 
 app.post('/authenticate', (req, res) => {
-  res.send(req.body);
+    res.send(req.body);
 });
 
 const listener = app.listen(process.env.PORT, function() {
-  console.log('Your app is listening on port ' + listener.address().port);
+    console.log('Your app is listening on port ' + listener.address().port);
 });
