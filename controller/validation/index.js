978b3c90af22ffc8ea01097f9158f8d0fc4345d9
Glitch (jelly-airport) <none>
2019-11-19 12:47:30 +0000

    🎄🏤 Checkpoint ./routes/auth.js:6368029/35 ./controller/validation/index.js:6368029/653

--- controller/validation/index.js
+++ controller/validation/index.js
@@ -1,12 +1,17 @@
 const Joi = require('@hapi/joi');
 
-const loginValidation = data => {
+const loginValidation = (req,_res,next) => {
     const schema = Joi.object({
         name: Joi.string().required(),
         password: Joi.string().required(),
     });
     
-    return schema.validate(data);
+    const {error} = schema.validate(req.body);
+    if(error) {
+        let err = new Error(error.details[0].message);
+        err.statusCode = 403;
+        next(err);
+    } else next();
 };
 
 module.exports = {
